3b8f4ffae9b3653d2f7d110345d8ad982b6729ed
fratertamas <fratertomi@gmail.com>
2020-09-19 08:47:00 +0200

    initial commit

---
+++ src/main/java/hu/frt/InstitutionQueryController.java
@@ -0,0 +1,9 @@
+package hu.frt;
+
+import hu.frt.EducationalInstitution;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InstitutionQueryController {
+
+}
