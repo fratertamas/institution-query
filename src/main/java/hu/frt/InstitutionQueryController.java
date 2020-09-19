6dcbe90b6d9e3ff63fc6fc3dc45c5425345cfc50
fratertamas <fratertomi@gmail.com>
2020-09-19 08:49:37 +0200

    add GetMapping institution-query

--- src/main/java/hu/frt/InstitutionQueryController.java
+++ src/main/java/hu/frt/InstitutionQueryController.java
@@ -1,9 +1,17 @@
 package hu.frt;
 
 import hu.frt.EducationalInstitution;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class InstitutionQueryController {
-
+    @GetMapping("institution-query")
+    public EducationalInstitution[] query(@RequestParam String omIdentificationNumber) {
+        final String uri = "http://localhost:8083/institution-read?omIdentificationNumber="+omIdentificationNumber;
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(uri,EducationalInstitution[].class);
+    }
 }
