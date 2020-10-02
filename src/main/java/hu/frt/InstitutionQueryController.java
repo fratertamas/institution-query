490266ed732b0768887d8925fff5755a17e209bc
fratertamas <fratertomi@gmail.com>
2020-10-02 13:31:27 +0200

    add profiles (dev, prod)

--- src/main/java/hu/frt/InstitutionQueryController.java
+++ src/main/java/hu/frt/InstitutionQueryController.java
@@ -1,6 +1,6 @@
 package hu.frt;
 
-import hu.frt.EducationalInstitution;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +8,33 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class InstitutionQueryController {
+    @Value("${spring.profiles.active}")
+    private String selectedProfile;
+
+    @Value("${hostname}")
+    private String hostname;
+
     @GetMapping("institution-query")
     public EducationalInstitution[] query(@RequestParam String omIdentificationNumber) {
-        final String uri = "http://institution-persistence:8083/institution-read?omIdentificationNumber="+omIdentificationNumber;
+        final String uri = "http://"+hostname+":8083/institution-read?omIdentificationNumber="+omIdentificationNumber;
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(uri,EducationalInstitution[].class);
     }
+
+    public String getSelectedProfile() {
+        return selectedProfile;
+    }
+
+    public void setSelectedProfile(String selectedProfile) {
+        this.selectedProfile = selectedProfile;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
 }
